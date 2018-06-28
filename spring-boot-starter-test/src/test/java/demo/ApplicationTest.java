9256ba94fe1d8515fa9efee5ef7e8f384f896aeb
jiangnenghua <jiangnenghua1992@gmail.com>
2018-06-28 13:47:05 +0800

    Merge remote-tracking branch 'origin/master' into release
    # Conflicts:
    #	pom.xml

--- spring-boot-starter-test/src/test/java/demo/ApplicationTest.java
+++ spring-boot-starter-test/src/test/java/demo/ApplicationTest.java
@@ -1,21 +1,8 @@
 package demo;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.*;
 
 /**
  * @author fibbery
@@ -25,63 +12,5 @@ import java.util.*;
 @SpringBootTest(classes = Application.class)
 public class ApplicationTest {
 
-    @Test
-    public void testSendMessage() {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "127.0.0.1:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        String topic = "testReceive";
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, "name", "jiangnenghua");
-        producer.send(record);
-    }
-
-    @Test
-    public void testGetMessage() {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "127.0.0.1:9092");
-        props.put("group.id", "ceshi4");
-        props.put("auto.offset.reset", "earliest");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-//        props.put("enable.auto.commit", "false");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-        consumer.subscribe(Collections.singleton("testReceive"));
-//        Map<TopicPartition, OffsetAndMetadata> currentOffset = new HashMap<>();
-//        AtomicInteger sequnce = new AtomicInteger(1);
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.printf("topic = %s, partition = %s, offset = %d, key = %s, value = %s\n",
-                        record.topic(), record.partition(), record.offset(), record.key(), record.value());
-//                currentOffset.put(new TopicPartition(record.topic(), record.partition()), new OffsetAndMetadata(record.offset() + 1, sequnce.toString()));
-            }
-        }
-    }
-
-    @Test
-    public void testDemo() {
-        System.out.println(6%6);
-    }
-
-    @Test
-    public void testIp() throws UnknownHostException, SocketException {
-        Set<InetAddress> addrs = new HashSet<InetAddress>();
-        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = networkInterfaces.nextElement();
-            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-            while (inetAddresses.hasMoreElements()) {
-                InetAddress inetAddress = inetAddresses.nextElement();
-                if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress() && !inetAddress.isMulticastAddress()) {
-                    addrs.add(inetAddress);
-                }
-            }
-        }
-        addrs.stream().forEach(address -> {
-            System.out.println(address.getHostAddress());
-        });
-    }
 }
 
