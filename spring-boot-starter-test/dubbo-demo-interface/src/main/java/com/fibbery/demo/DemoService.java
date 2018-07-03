c1677d55bba0c8bda553d56d9af55061a76dbb18
jiangnenghua <jiangnenghua1992@gmail.com>
2018-07-03 15:49:10 +0800

    梳理项目结构

--- spring-boot-starter-test/dubbo-demo-interface/src/main/java/com/fibbery/demo/DemoService.java
+++
@@ -1,8 +0,0 @@
-package com.fibbery.demo;
-
-/**
- * @author fibbery
- * @date 18/7/2
- */
-public class DemoService {
-}
--- spring-boot-starter-test/dubbo-demo-provider/src/main/java/com/fibbery/demo/provider/DemoServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.fibbery.demo.provider;
-
-/**
- * @author fibbery
- * @date 18/7/2
- */
-public class DemoServiceImpl {
-}
